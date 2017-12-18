e171d9b9a48496f52b785b06466263bd20e8050b
ryanc1993 <ryan149347@gmail.com>
2017-12-18 16:29:19 +0800

    + report

--- hw5/parseData.py
+++ hw5/parseData.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import sys, os
 
-def read_data(dataPath = './data/train.csv'):
+def read_data(dataPath):
     # MAX_USERID: 6040
     # MAX_MOVIEID: 3952
     # UserID: [ 795  795  795 ..., 2682 2682 2682], shape = (899873,)
@@ -21,7 +21,7 @@ def read_data(dataPath = './data/train.csv'):
 
     return MAX_USERID, MAX_MOVIEID, UserID, MovieID, Rating
 
-def read_test_data(dataPath = './data/test.csv'):
-    test_data = pd.read_csv('./data/test.csv', usecols = ['UserID', 'MovieID'])
-    recommendations = pd.read_csv('./data/test.csv', usecols = ['TestDataID'])
+def read_test_data(dataPath):
+    test_data = pd.read_csv(dataPath, usecols = ['UserID', 'MovieID'])
+    recommendations = pd.read_csv(dataPath, usecols = ['TestDataID'])
     return test_data, recommendations
--- hw5/predict.py
+++ hw5/predict.py
@@ -9,9 +9,9 @@ from deep_model import Deep_Model
 def predict_rating(model, userid, movieid):
     return model.predict([np.array([userid-1]), np.array([movieid-1])])[0][0]
 
-def main():
+def main(argv):
     EMBEDDING_DIM = 200
-    test_data, recommendations = parseData.read_test_data()
+    test_data, recommendations = parseData.read_test_data(argv[1])
     model = CF_Model(6040, 3952, EMBEDDING_DIM)
     model.load_weights('./model/model_unbias.h5')
     model1 = CF_Model(6040, 3952, EMBEDDING_DIM)
@@ -24,8 +24,10 @@ def main():
     recommendations['Rating_1'] = test_data.apply(lambda x: predict_rating(model, x['UserID'], x['MovieID']), axis = 1)
     recommendations['Rating_2'] = test_data.apply(lambda x: predict_rating(model1, x['UserID'], x['MovieID']), axis = 1)
     recommendations['Rating'] = (recommendations['Rating_1'] + recommendations['Rating_2']) / 2
-    recommendations.to_csv('resultTest.csv', index=False, columns=['TestDataID', 'Rating'])
+    filename = argv[2]
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    recommendations.to_csv(filename, index=False, columns=['TestDataID', 'Rating'])
     print(recommendations)
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
